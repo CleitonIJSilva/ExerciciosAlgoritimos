5137cb8fbe768c19533f389978ef7381dfb7035f
cleitonIJSilva <cleitonjoaquimti@gmail.com>
2023-09-18 15:59:59 -0300

    second commit

--- quadrado.js
+++ quadrado.js
@@ -2,6 +2,6 @@ const prompt = require('prompt-sync')();
 
 let numero = prompt("Digite um numero: ")
 
-let quadrado = numero * numero
+let quadrado = numero ** 2 // **2 significa ao quadrado
 
 console.log(quadrado)
